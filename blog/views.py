1d6d2067f040ca0c9dbdc937d54e5169850ec9a4
Elif <eliifhsgl@gmail.com>
2016-05-07 15:08:42 +0300

    Detayli blog gonderileri icin css'e ilaveten view ve template eklendi.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-def post_list(request):     
-    return render(request, 'blog/post_list.html', {})
+from django.utils import timezone
+from .models import Post
+from django.shortcuts import get_object_or_404
+
+def post_list(request):
+    posts = Post.objects.filter(yayinlama_tarihi__lte=timezone.now()).order_by('yayinlama_tarihi')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})   
